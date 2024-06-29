861aced8203382f7aa6fc2041cc7d4a66a12c6a6
kirannandi896 <kirannandi896@gmail.com>
2024-06-29 12:44:50 +0530

    29 june

---
+++ script.js
@@ -0,0 +1,24 @@
+function display_hidden(){
+    if (document.getElementById("hidden").style.display == "none"){
+      document.getElementById("hidden").style.display = "block";
+    }
+    else{
+      document.getElementById("hidden").style.display = "none"
+    }
+  }
+  function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
+}
+function myFun() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
+}
